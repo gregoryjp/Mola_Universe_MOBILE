613e364e818e729746428d00197f42d3569e4e66
Gregory <gregory.jp.dev@gmail.com>
2026-09-16 16:18:19 +0200

    feat(app): wire up providers and navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import type { JSX } from 'react';
+import { RootNavigator } from '@core/navigation/RootNavigator';
+import { QueryProvider } from '@core/query/QueryProvider';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
+export default function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <QueryProvider>
+        <RootNavigator />
+      </QueryProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
